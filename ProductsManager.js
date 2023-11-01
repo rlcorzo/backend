c05225371c936e970ac78cf133bc8e4d45255f11
rcorzo <corzorodrigo0@gmail.com>
2023-11-01 15:50:43 -0300

    Entrega4_Rcorzo

--- ProductsManager.js
+++ ProductsManager.js
@@ -1,65 +1,119 @@
-import { error } from 'console'
-import { json } from 'express'
+import {
+    error
+} from 'console'
+import {
+    json
+} from 'express'
 import fs from 'fs'
 
 
 
-class ProductsManager{
-    constructor(path){
-        this.path=path
+class ProductsManager {
+    constructor(path) {
+        this.path = path
     }
 
-async getProducts(queryObj){
-    const {limit} = queryObj
-    try{
-
-        if(fs.existsSync(this.patch)){
-            const productsFile = await fs.promises.readFile(this.patch, 'utf-8')
-            const productsArray = JSON.parse(productsFile)
-            return limit ? productsArray.slice(0,limit) : productsArray
-        }else {
-            return []
-        }
-
-    }catch (error) {
-        return error
-    }
-
-}
-
-    async getProductsId(id){
+    async getProducts() {
         try {
-            const productsFile = await this.getProducts({})
-            const product = productsFile.find(p=>p.id===id)
-            return product
-
-        } catch(error){
-            return error
-       }
-    }
-
-
-    async createProduct(obj){
-        try{
-            const products = await this.getProducts({})
-            let id
-            if (!products.length){
-                id = 1
-            }else{
-                id = products[products.length -1].id +1
+            if (fs.existsSync(this.patch)) {
+                const Products = await fs.promises.readFile(this.patch, 'utf-8')
+                return JSON.parse(products);
+            } else {
+                return [];
             }
-            const newProduct = { id, ...obj }
-            products.push(newProduct)
-            await fs.promises.writeFile(this.path, JSON.stringify(products))
-            return newProduct
 
         } catch (error) {
-            return error
+            return error;
+        }
+
+    }
+
+
+
+    async addProduct(title, description, price, thumbnail, code, stock) {
+        try {
+            const products = await this.getProducts();
+            if (!title || !description || !price || !thumbnail || !code || !stock) {
+                return console.log("error: All fields are required");
+            } else if (products.some((product) => product.code === code.trim())) {
+                return console.log("error: Product code already exists");
+            } else {
+                const product = {
+                    id: products.length ? products[products.length - 1].code + 1 : 1,
+                    title: title.trim(),
+                    description: description.trim(),
+                    price: price,
+                    thumbnail: thumbnail.trim(),
+                    code: code.trim(),
+                    stock: stock,
+                };
+                products.push(product);
+                await fs.promises.writeFile(this.path, JSON.stringify(products));
+            }
+        } catch (error) {
+            return error;
         }
     }
 
 
+    async getProductById(id) {
+        try {
+            const products = await this.getProducts();
+            return (
+                products.find((product) => product.id == id) || {
+                    error: "Product Not Found",
+                }
+            );
+        } catch (error) {
+            return error;
+        }
+    }
+
+
+
+    async updateProductById(id, data) {
+        try {
+            const products = await this.getProducts();
+            const productIndex = products.findIndex((product) => product.id == id);
+            if (productIndex === -1) {
+                return {
+                    error: "Product Not Found"
+                };
+            }
+
+            if (data.id && data.id !== id) {
+                throw new Error("Updating the product ID is not allowed");
+            }
+
+            const updatedProduct = {
+                ...products[productIndex],
+                ...data
+            };
+            products[productIndex] = updatedProduct;
+            await fs.promises.writeFile(this.path, JSON.stringify(products));
+            return updatedProduct;
+        } catch (error) {
+            return error;
+        }
+    }
+
+
+
+
+    async deleteProductById(id) {
+        try {
+            const products = await this.getProducts();
+            const newProductsArray = products.filter((product) => product.id != id);
+            await fs.promises.writeFile(this.path, JSON.stringify(newProductsArray));
+        } catch (error) {
+            return error;
+        }
+    }
 }
+
+
+
+
 
 const product1 = {
     name: 'Gaseosa',
@@ -92,4 +146,4 @@ const product5 = {
 }
 */
 
-export const productManager = new ProductsManager ();
+export const productManager = new ProductManager("productsAPI.json");
--- app.js
+++ app.js
@@ -1,22 +1,30 @@
 import express from 'express';
-import {
-    productManager
-} from './ProductsManager.js';
+import { productManager} from './ProductsManager.js';
+
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
 
-app.get('/api/products', async (req, res) => {
+
+app.get("/", (req,res) =>{
+    res.send ("Bienvenidos al servidor de productos");
+});
+
+app.get("/products", async (req, res) => {
     try {
-        const products = await productManager.getProducts(req.query);
+        const products = await productManager.getProducts();
+        const {limit} = req.query;
         if (!products.length) {
-            return res.status(200).json({
-                message: 'No products'
-            });
+            res.status(200).json({ message: 'No Products Found'});
+        }else{
+            if (limit) {
+                res.status(200).json(products.slice(0,limit));
+            }else{
+                res.status(200).json({message: "Products found", products});
+            }
+
         }
-        res.status(200).json({
-            message: 'Products found',
-            products
-        });
     } catch (error) {
         res.status(500).json({
             message: error.message
@@ -24,28 +32,20 @@ app.get('/api/products', async (req, res) => {
     }
 });
 
-app.get('/api/products/:idProduct', async (req, res) => {
-    const {
-        idProduct
-    } = req.params
+app.get("/products/:pid", async (req, res) => {
+    const {pid} = req.params;
     try {
-        const product = await productManager.getProductById(+idProduct)
-        if (!product) {
-            return res.status(400).json({
-                message: 'Product not found with the id'
-            })
+        const product = await productManager.getProductById(pid);
+        if (product.error === "Product Not Found") {
+             res.status(404).json({ message: "Product not found" })
+        }else{
+            res.status(200).json({ message: "Product found",product});
         }
-        res.status(200).json({
-            message: "Product found",
-            product
-        });
 
     } catch (error) {
-        res.status(500).json({
-            message: error.message
-        });
+        res.status(500).json({ message: "Internal Server Error"});
     }
-})
+});
 
 
 
