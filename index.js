06520ba79503b06f715f6ef924726b51ef5fd0af
rcorzo <corzorodrigo0@gmail.com>
2023-11-01 18:02:04 -0300

    Entrega5

--- index.js
+++
@@ -1,6 +0,0 @@
-import http from 'http'
-const server = http.createServer()
-
-server.listen(8080,()=>{
-    console.log('Escuchando al puerto 8080 - HTTP ')
-})
--- src/server.js
+++ src/server.js
@@ -1,9 +1,48 @@
 import express from "express"
+import {productManager} from './ProductsManager.js';
 import productsRouter from './router/products.router.js'
 import cartsRouter from './router/carts.router.js'
 import { __dirname } from "./utils.js"
 
 const app = express()
+
+app.get('/api/products', async (req, res) => {
+    try {
+        const products = await productManager.getProducts(req.query);
+        if (!products.length) {
+            return res.status(200).json({
+                message: 'No products'
+            });
+        }
+        res.status(200).json({
+            message: 'Products found',
+            products
+        });
+    } catch (error) {
+        res.status(500).json({ message: "Internal Server Error"});
+    }
+});
+
+app.get('/api/products/:idProduct', async (req, res) => {
+    const {
+        idProduct
+    } = req.params
+    try {
+        const product = await productManager.getProductById(+idProduct)
+        if (!product) {
+            return res.status(400).json({
+                message: 'Product not found with the id'
+            })
+        }
+        res.status(200).json({
+            message: "Product found",
+            product
+        });
+
+    } catch (error) {
+        res.status(500).json({ message: "Internal Server Error"});
+    }
+})
 
 
 
